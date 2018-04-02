polgreen/uclid

commit f790569d866bda19bc2b73c76e2726f119998b90

Better CEX printing.

src/main/scala/lang/UclidLanguage.scala
@@ -761,7 +761,7 @@ case class ProcedureCallStmt(id: Identifier, callLhss: List[Lhs], args: List[Exp
     Utils.join(args.map(_.toString), ", ") + ") // " + id.position.toString)
 }
 case class ModuleCallStmt(id: Identifier) extends Statement {
-  override def toLines = List("call (" + id.toString +")")
+  override def toLines = List("next (" + id.toString +")")
 }
 case class LocalVarDecl(id: Identifier, typ: Type) extends ASTNode {
   override def toString = "var " + id + ": " + typ + "; // " + id.position.toString
@@ -1042,7 +1042,7 @@ case class AxiomDecl(id : Option[Identifier], expr: Expr) extends Decl {
 }
 sealed abstract class ProofCommand extends ASTNode
 
-case class GenericProofCommand(name : Identifier, params: List[Identifier], args : List[Expr], resultVar: Option[Identifier], argObj: Option[Identifier]) extends ProofCommand {
+case class GenericProofCommand(name : Identifier, params: List[Identifier], args : List[(Expr, String)], resultVar: Option[Identifier], argObj: Option[Identifier]) extends ProofCommand {
   def getContext(context : Scope) : Scope = {
     argObj match {
       case Some(arg) =>
@@ -1077,6 +1077,13 @@ case class GenericProofCommand(name : Identifier, params: List[Identifier], args
 
 sealed abstract class Annotation extends ASTNode
 case class InstanceVarMapAnnotation(iMap: Map[List[Identifier], Identifier]) extends Annotation {
+  lazy val rMap : Map[Identifier, String] = {
+    iMap.map {
+      p => {
+        p._2 -> Utils.join(p._1.map(id => id.toString()), "->")
+      }
+    }
+  }
   override def toString : String = {
     val start = PrettyPrinter.indent(1) + "// instance_var_map { "
     val lines = iMap.map(p => PrettyPrinter.indent(1) + "//   " + Utils.join(p._1.map(_.toString), "->") + " ::==> " + p._2.toString)
@@ -1161,6 +1168,7 @@ case class Module(id: Identifier, decls: List[Decl], cmds : List[GenericProofCom
     if (matchingNotes.size == 0) {
       None
     } else {
+      Utils.assert(matchingNotes.size == 1, "Too many annotations of type: " + tag.toString())
       Some(matchingNotes(0))
     }
   }

src/main/scala/SymbolicSimulator.scala
@@ -98,7 +98,7 @@ class SymbolicSimulator (module : Module) {
               case None    => "unroll"
             }
             initialize(false, true, false, context, label, noLTLFilter)
-            symbolicSimulate(cmd.args(0).asInstanceOf[IntLit].value.toInt, true, false, context, label, noLTLFilter)
+            symbolicSimulate(cmd.args(0)._1.asInstanceOf[IntLit].value.toInt, true, false, context, label, noLTLFilter)
           case "bmc" =>
             val label : String = cmd.resultVar match {
               case Some(l) => l.toString()
@@ -117,7 +117,7 @@ class SymbolicSimulator (module : Module) {
               ExprDecorator.isLTLProperty(decorators) &&  (cmd.params.isEmpty || cmd.params.contains(nameToCheck))
             }
             initialize(false, true, false, context, label, LTLFilter)
-            symbolicSimulate(cmd.args(0).asInstanceOf[IntLit].value.toInt, true, false, context, label, LTLFilter)
+            symbolicSimulate(cmd.args(0)._1.asInstanceOf[IntLit].value.toInt, true, false, context, label, LTLFilter)
           case "induction" =>
             val labelBase : String = cmd.resultVar match {
               case Some(l) => l.toString + ": induction (base)"
@@ -128,7 +128,7 @@ class SymbolicSimulator (module : Module) {
               case None    => "induction (step)"
             }
             val k = if (cmd.args.size > 0) {
-              cmd.args(0).asInstanceOf[IntLit].value.toInt
+              cmd.args(0)._1.asInstanceOf[IntLit].value.toInt
             } else { 1 }
 
             // base case.
@@ -149,7 +149,7 @@ class SymbolicSimulator (module : Module) {
             // go back to original state.
             resetState()
           case "verify" =>
-            val procName = cmd.args(0).asInstanceOf[Identifier]
+            val procName = cmd.args(0)._1.asInstanceOf[Identifier]
             val proc = module.procedures.find(p => p.id == procName).get
             val label : String = cmd.resultVar match {
               case Some(l) => l.toString + ": verify(%s)".format(procName.toString())
@@ -311,7 +311,7 @@ class SymbolicSimulator (module : Module) {
     }
   }
 
-  def printCEX(results : List[CheckResult], exprs : List[Expr], arg : Option[Identifier]) {
+  def printCEX(results : List[CheckResult], exprs : List[(Expr, String)], arg : Option[Identifier]) {
     def labelMatches(p : AssertInfo) : Boolean = {
       arg match {
         case Some(id) => id.toString == p.label || p.label.startsWith(id.toString + ":")
@@ -325,12 +325,21 @@ class SymbolicSimulator (module : Module) {
     })
   }
 
-  def printCEX(res : CheckResult, exprs : List[Expr]) {
+  def printCEX(res : CheckResult, exprs : List[(Expr, String)]) {
     println("CEX for %s".format(res.assert.toString, res.assert.pos.toString))
     val scope = res.assert.context
-    val exprsToPrint = if (exprs.size == 0) {
-      val vars = (scope.inputs ++ scope.vars ++ scope.outputs).map(_.id)
-      vars.toList.sortWith((l, r) => l.name < r.name)
+    lazy val instVarMap = module.getAnnotation[InstanceVarMapAnnotation]().get
+
+    val exprsToPrint : List[(Expr, String)] = if (exprs.size == 0) {
+      val vars = ((scope.inputs ++ scope.vars ++ scope.outputs).map { 
+        p => {
+          instVarMap.rMap.get(p.id) match {
+            case Some(str) => (p.id, str)
+            case None => (p.id, p.id.toString)
+          }
+        }
+      })
+      vars.toList.sortWith((l, r) => l._2 < r._2)
     } else {
       exprs
     }
@@ -351,7 +360,7 @@ class SymbolicSimulator (module : Module) {
     throw new Utils.UnimplementedException("Implement print_smt2.")
   }
 
-  def printFrame(f : SymbolTable, pastFrames : Map[Int, SymbolTable], m : smt.Model, exprs : List[Expr], scope : Scope) {
+  def printFrame(f : SymbolTable, pastFrames : Map[Int, SymbolTable], m : smt.Model, exprs : List[(Expr, String)], scope : Scope) {
     def expr(id : lang.Identifier) : Option[smt.Expr] = {
       if (f.contains(id)) { Some(evaluate(id, f, pastFrames, scope)) }
       else { None }
@@ -359,8 +368,8 @@ class SymbolicSimulator (module : Module) {
 
     exprs.foreach { (e) => {
       try {
-        val result = m.evalAsString(evaluate(e, f, pastFrames, scope))
-        println("  " + e.toString + " : " + result)
+        val result = m.evalAsString(evaluate(e._1, f, pastFrames, scope))
+        println("  " + e._2 + " : " + result)
       } catch {
         case excp : Utils.UnknownIdentifierException =>
           println("  " + e.toString + " : <UNDEF> ")

src/main/scala/lang/ASTVistors.scala
@@ -471,7 +471,7 @@ class ASTAnalyzer[T] (_passName : String, _pass: ReadOnlyPass[T]) extends ASTAna
     var result : T = in
     val contextP = cmd.getContext(context + cmd)
     result = pass.applyOnCmd(TraversalDirection.Down, cmd, result, context)
-    result = cmd.args.foldLeft(result)((r, expr) => visitExpr(expr, r, contextP))
+    result = cmd.args.foldLeft(result)((r, expr) => visitExpr(expr._1, r, contextP))
     result = cmd.resultVar match {
       case Some(id) => visitIdentifier(id, result, contextP)
       case None => result
@@ -1199,7 +1199,15 @@ class ASTRewriter (_passName : String, _pass: RewritePass, setFilename : Boolean
 
   def visitCommand(cmd : GenericProofCommand, context : Scope) : Option[GenericProofCommand] = {
     val contextP = cmd.getContext(context + cmd)
-    val argsP = cmd.args.map(e => visitExpr(e, contextP)).flatten
+    val argsP = (cmd.args.map { 
+      e => {
+        val eP = visitExpr(e._1, contextP)
+        eP match {
+          case Some(ePP) => Some(ePP, e._2)
+          case None => None
+        }
+      }
+    }).flatten
     val resultVarP = cmd.resultVar.flatMap(r => visitIdentifier(r, contextP))
     val argObjP = cmd.argObj.flatMap(r => visitIdentifier(r, contextP))
     val cmdP = pass.rewriteCommand(GenericProofCommand(cmd.name, cmd.params, argsP, resultVarP, argObjP), context)

src/main/scala/lang/ControlCommandChecker.scala
@@ -59,22 +59,22 @@ class ControlCommandCheckerPass extends ReadOnlyPass[Unit] {
   def checkHasOneIntLitArg(cmd : GenericProofCommand, filename : Option[String]) {
     Utils.checkParsingError(cmd.args.size == 1, "'%s' command expects exactly one argument".format(cmd.name.toString), cmd.pos, filename)
     val cntLit = cmd.args(0)
-    Utils.checkParsingError(cntLit.isInstanceOf[IntLit], "'%s' command expects a constant integer argument".format(cmd.name.toString), cmd.pos, filename)
-    val cnt = cntLit.asInstanceOf[IntLit].value
+    Utils.checkParsingError(cntLit._1.isInstanceOf[IntLit], "'%s' command expects a constant integer argument".format(cmd.name.toString), cmd.pos, filename)
+    val cnt = cntLit._1.asInstanceOf[IntLit].value
     val cntInt = cnt.intValue()
     Utils.checkParsingError(cntInt == cnt, "Argument to '%s' is too large".format(cmd.name.toString), cmd.pos, filename)
   }
   def checkHasOneIdentifierArg(cmd : GenericProofCommand, filename : Option[String]) {
     Utils.checkParsingError(cmd.args.size == 1, "'%s' command expects exactly one argument".format(cmd.name.toString), cmd.pos, filename)
     val cntLit = cmd.args(0)
-    Utils.checkParsingError(cntLit.isInstanceOf[Identifier], "'%s' command expects a identifier as argument".format(cmd.name.toString), cmd.pos, filename)
+    Utils.checkParsingError(cntLit._1.isInstanceOf[Identifier], "'%s' command expects a identifier as argument".format(cmd.name.toString), cmd.pos, filename)
   }
   def checkHasZeroOrOneIntLitArg(cmd : GenericProofCommand, filename : Option[String]) {
     Utils.checkParsingError(cmd.args.size <= 1, "'%s' command expects no more than one argument".format(cmd.name.toString), cmd.pos, filename)
     if (cmd.args.size > 0) {
       val cntLit = cmd.args(0)
-      Utils.checkParsingError(cntLit.isInstanceOf[IntLit], "'%s' command expects a constant integer argument".format(cmd.name.toString), cmd.pos, filename)
-      val cnt = cntLit.asInstanceOf[IntLit].value
+      Utils.checkParsingError(cntLit._1.isInstanceOf[IntLit], "'%s' command expects a constant integer argument".format(cmd.name.toString), cmd.pos, filename)
+      val cnt = cntLit._1.asInstanceOf[IntLit].value
       val cntInt = cnt.intValue()
       Utils.checkParsingError(cntInt == cnt, "Argument to '%s' is too large".format(cmd.name.toString), cmd.pos, filename)
     }
@@ -119,7 +119,7 @@ class ControlCommandCheckerPass extends ReadOnlyPass[Unit] {
         checkNoParams(cmd, filename)
         checkHasOneIdentifierArg(cmd, filename)
         checkNoArgObj(cmd, filename)
-        val arg = cmd.args(0).asInstanceOf[Identifier]
+        val arg = cmd.args(0)._1.asInstanceOf[Identifier]
         val module = context.module.get
         lazy val errorMsg = "Unknown procedure: '%s'".format(arg.toString())
         Utils.checkParsingError(module.procedures.find(p => p.id == arg).isDefined, errorMsg, arg.pos, filename)

src/main/scala/lang/ModuleFlattener.scala
@@ -132,7 +132,7 @@ class ModuleInstantiatorPass(module : Module, inst : InstanceDecl, targetModule 
   def createVarMap() : (VarMap, ExternalSymbolMap) = {
     // sanity check
     Utils.assert(targetModule.instances.size == 0, "All instances in target module must have been flattened by now. Module: %s. Instance: %s.\n%s".format(module.id.toString, inst.toString, targetModule.toString))
-    val nameProvider = new ContextualNameProvider(Scope.empty + module, "$inst:" + inst.instanceId.toString)
+    val nameProvider = new ContextualNameProvider(Scope.empty + module, "_inst_" + inst.instanceId.toString)
 
     val idMap0 : VarMap = Map.empty
     // map each input

src/main/scala/lang/PrimedVariableEliminator.scala
@@ -44,7 +44,7 @@ class PrimedVariableCollectorPass extends ReadOnlyPass[(Map[Identifier, Identifi
   type T = (Map[Identifier, Identifier], Option[ContextualNameProvider])
   override def applyOnModule(d : TraversalDirection.T, module : Module , in : T, context : Scope) : T = {
     if (d == TraversalDirection.Down) {
-      val nameProvider = new ContextualNameProvider(context, "prime_")
+      val nameProvider = new ContextualNameProvider(context, "_prime_")
       (in._1, Some(nameProvider))
     } else {
       (in._1, None)

src/main/scala/lang/UclidParser.scala
@@ -621,9 +621,9 @@ object UclidParser extends UclidTokenParsers with PackratParsers {
       (Id <~ "=").? ~ (Id <~ "->").? ~ Id ~ IdParamList <~ ";" ^^
         { case rId ~ oId ~ id ~ idparams => lang.GenericProofCommand(id, idparams, List.empty, rId, oId) } |
       (Id <~ "=").? ~ (Id <~ "->").? ~ Id ~ ExprList <~ ";" ^^
-        { case rId ~ oId ~ id ~ es => lang.GenericProofCommand(id, List.empty, es, rId, oId) } |
+        { case rId ~ oId ~ id ~ es => lang.GenericProofCommand(id, List.empty, es.map(e => (e, e.toString())), rId, oId) } |
       (Id <~ "=").? ~ (Id <~ "->").? ~ Id ~ IdParamList ~ ExprList <~ ";" ^^
-        { case rId ~ oId ~ id ~ idparams ~ es => lang.GenericProofCommand(id, idparams, es, rId, oId) }
+        { case rId ~ oId ~ id ~ idparams ~ es => lang.GenericProofCommand(id, idparams, es.map(e => (e, e.toString())), rId, oId) }
     }
 
     lazy val CmdBlock : PackratParser[List[GenericProofCommand]] = KwControl ~ "{" ~> rep(Cmd) <~ "}"

src/test/scala/VerifierSpec.scala
@@ -165,6 +165,9 @@ class ModuleVerifSpec extends FlatSpec {
   "test-modules-1.ucl" should "verify all assertions." in {
     VerifierSpec.expectedFails("./test/test-modules-1.ucl", 0)
   }
+  "test-modules-5.ucl" should "verify all assertions." in {
+    VerifierSpec.expectedFails("./test/test-modules-5.ucl", 0)
+  }
   "test-type-import.ucl" should "verify all assertions." in {
     VerifierSpec.expectedFails("./test/test-type-import.ucl", 0)
   }

commit 9923d5184203d0853bdda42985de5f2323547196

Fixing procedure cex printing.
This got broken with f790569d866bda19bc2b73c76e2726f119998b90 when we
tried to make cexs better for modules.

src/main/scala/lang/TypeChecker.scala
@@ -235,6 +235,8 @@ class ExpressionTypeCheckerPass extends ReadOnlyPass[Set[Utils.TypeError]]
   override def applyOnExpr(d : TraversalDirection.T, e : Expr, in : ErrorList, ctx : Scope) : ErrorList = {
     if (d == TraversalDirection.Up) {
       try {
+        logger.debug("expression: {}", e.toString())
+        logger.debug("ctx.map: {}", ctx.map.toString())
         typeOf(e, ctx)
       } catch {
         case p : Utils.TypeError => {

src/main/scala/lang/UclidLanguage.scala
@@ -1074,7 +1074,7 @@ case class GenericProofCommand(name : Identifier, params: List[Identifier], args
           val mod = context.module.get
           val verifCmd = context.get(arg).get.asInstanceOf[Scope.VerifResultVar].cmd
           if (verifCmd.isVerify) {
-            val procName = verifCmd.args(0).asInstanceOf[Identifier]
+            val procName = verifCmd.args(0)._1.asInstanceOf[Identifier]
             val proc = mod.procedures.find(p => p.id == procName).get
             context + proc
           } else {